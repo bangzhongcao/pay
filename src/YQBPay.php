99071a63a434c1c776b3a958cb4df71e96557552
guochao_xian <464904154@qq.com>
2019-07-30 11:43:03 +0800

    新增平安支付

--- src/YQBPay.php
+++ src/YQBPay.php
@@ -63,11 +63,11 @@ class YQBPay extends BaseObject
 
     /**
      * YQBPay constructor.
-     * @param string $merchantId
-     * @param string $merchantKey
-     * @param string $notifyUrl
-     * @param string $redirectUrl
-     * @param string $failbackUrl
+     * @param string $merchantId 商户号
+     * @param string $merchantKey 商户密钥
+     * @param string $notifyUrl 支付结果异步通知地址
+     * @param string $redirectUrl 前台页面跳转地址
+     * @param string $failbackUrl 失败跳转地址
      */
     public function __construct(string $merchantId, string $merchantKey, string $notifyUrl, string $redirectUrl, string $failbackUrl)
     {
@@ -240,12 +240,12 @@ class YQBPay extends BaseObject
 
     /**
      * 微信聚合收银台落单
-     * @param string $order
-     * @param int $amount
-     * @param null $body
-     * @param array $otherParams
-     * @param array $merReserved
-     * @param array $riskInfo
+     * @param string $order 订单号
+     * @param int $amount 订单金额
+     * @param null $body 订单说明
+     * @param array $otherParams 其他数据
+     * @param array $merReserved 商户保留域
+     * @param array $riskInfo 风控信息
      * @return array
      */
     public function wechatCashierRevOrder(string $order, int $amount, $body = null, $otherParams = [], $merReserved = [], $riskInfo = [])
@@ -279,8 +279,8 @@ class YQBPay extends BaseObject
 
     /**
      * 微信聚合收银台005通用交易查询
-     * @param string $order
-     * @param array $otherParams
+     * @param string $order 订单号
+     * @param array $otherParams 其他数据
      * @return array
      */
     public function wechatCashierQuery(string $order, $otherParams = [])
@@ -304,7 +304,7 @@ class YQBPay extends BaseObject
 
     /**
      * 回调校验签名
-     * @param array $params
+     * @param array $params 通知参数
      * @return array
      */
     public function wechatCashierNotifyVerify(array $params)
